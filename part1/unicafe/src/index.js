25f566ef1827b80f291a33dde7078f18c55a3f5f
unknown <hola@jlpalacios.es>
2023-11-16 13:10:12 +0100

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -31,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>newtral {newtral}</p>
       <p>bad {bad}</p>
+      <p>all {good + newtral + bad}</p>
+      <p>average {(good-bad) / (good + newtral + bad)}</p>
+      <p>positive {(good * 100) / (good + newtral + bad)} %</p>
     </div>
   )
 }
