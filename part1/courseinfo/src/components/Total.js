d0c7ae562fd643431b33dcbef67e08e91f577c34
unknown <hola@jlpalacios.es>
2023-11-16 12:17:48 +0100

    1.1

---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>Numbers of exercises {exercises1 + exercises2 + exercises3}</div>
+  )
+}
