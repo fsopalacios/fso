d1edef935185448f2661f65f331c61224285b6e7
unknown <hola@jlpalacios.es>
2023-11-16 13:19:20 +0100

    1.9

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -4,12 +4,19 @@ export const Statistics = ({ good, newtral, bad }) => {
   return (
     <div>
         <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>newtral {newtral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + newtral + bad}</p>
-        <p>average {(good-bad) / (good + newtral + bad)}</p>
-        <p>positive {(good * 100) / (good + newtral + bad)} %</p>
+        {good+newtral+bad === 0 
+            ? 
+                <p>No feedback given</p> 
+            : 
+                <>
+                    <p>good {good}</p>
+                    <p>newtral {newtral}</p>
+                    <p>bad {bad}</p>
+                    <p>all {good + newtral + bad}</p>
+                    <p>average {(good-bad) / (good + newtral + bad)}</p>
+                    <p>positive {(good * 100) / (good + newtral + bad)} %</p>
+                </>
+        }
     </div>
   )
 }
