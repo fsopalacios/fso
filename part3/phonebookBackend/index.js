75f895d1e06ab3eff392e0ff52d0e7ecaf64328f
unknown <hola@jlpalacios.es>
2023-12-03 12:16:38 +0100

    3.1

---
+++ part3/phonebookBackend/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const phones = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "asdf",
+      "number": "asdf",
+      "id": 5
+    },
+    {
+      "name": "1111",
+      "number": "3333",
+      "id": 6
+    }
+  ]
+
+app.get('/', (req, res) => {
+    res.json({
+        estado:true,
+        mensaje:"phonebookBackend"
+    })
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(phones)
+})
+
+const PORT = 8002
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT ${PORT}`)
+})
